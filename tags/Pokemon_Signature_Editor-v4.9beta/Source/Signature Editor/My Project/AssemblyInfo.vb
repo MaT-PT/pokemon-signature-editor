4d68c772f02dc95e02d24eca7ca61f7fbcfdb567
Mat <MaT-PT@users.noreply.github.com>
2010-10-09 19:53:03 +0000

    Tagging version 4.9beta.
    git-svn-id: svn://localhost/home/mat/projets/pokemon-signature-editor/pokemon-signature-editor_svn@4 98164a29-962a-4dce-8f92-5cef09f52806

---
+++ tags/Pokemon_Signature_Editor-v4.9beta/Source/Signature Editor/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Pokémon Signature Editor")> 
+<Assembly: AssemblyDescription("Éditeur de Signature pour les jeux Pokémon 4G sur NDS")> 
+<Assembly: AssemblyCompany("M@T")> 
+<Assembly: AssemblyProduct("Pokémon Signature Editor")> 
+<Assembly: AssemblyCopyright("M@T 2009-2010 - PokemonTrash.com")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("40029eef-cb57-4994-be35-1c93a3e5a69a")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.9.2010.0910")> 
+<Assembly: AssemblyFileVersion("4.9.2010.0910")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("fr-FR")> 
